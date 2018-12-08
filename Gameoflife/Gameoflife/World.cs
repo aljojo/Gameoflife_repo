32a4a6f028d87e9eaa3e48b11bac354662387163
Alexander <Alexander@LAPTOP-1396SG1K>
2018-12-08 11:46:03 +0100

    Added cell class.

---
+++ Gameoflife/Gameoflife/World.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gameoflife
+{
+    class World
+    {
+        Int64 width;
+        Int64 length;
+        Double time_per_evolution_step;
+
+    }
+}
---
+++ Gameoflife/Gameoflife/cell.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gameoflife
+{
+    class cell
+    {
+        Boolean isalive;
+        Boolean willbealive;
+
+        public cell()
+        {
+            isalive = false;
+            willbealive = false;
+        }
+
+        public Boolean is_cell_alive_now()
+        {
+            return isalive;
+        }
+
+        public void set_cell_to_be_alive(Boolean yes_set_to_alive)
+        {
+            willbealive = yes_set_to_alive;
+        }
+    }
+}
