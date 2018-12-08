ce0f4f3772e6014464d340a9627e7d87a6d751e5
Alexander <Alexander@LAPTOP-1396SG1K>
2018-12-08 18:34:29 +0100

    Graphical Output prototype added

---
+++ Gameoflife/Gameoflife/GraphicalDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Gameoflife
+{
+    public class GraphicalDisplay : Form
+    {
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            Graphics g = e.Graphics;
+            using (Pen selPen = new Pen(Color.Blue))
+            {
+                g.DrawRectangle(selPen, 10, 10, 50, 50);
+            }
+        }
+    }
+}
--- Gameoflife/Gameoflife/Program.cs
+++ Gameoflife/Gameoflife/Program.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+
 
 namespace Gameoflife
 {
@@ -14,10 +14,16 @@ namespace Gameoflife
         [STAThread]
         static void Main()
         {
-            //Application.EnableVisualStyles();
-            //Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
-            World world = new World(128, 128, 0.1);
+            Int64 world_width = 128;
+            Int64 world_length = 128;
+            Double timestep = 0.1;
+            World world = new World( world_width, world_length, timestep);
+
+            GraphicalDisplay graphics = new GraphicalDisplay();
+            graphics.Show();
+            //System.Threading.Thread.Sleep(5000);
         }
     }
 }
+
+
